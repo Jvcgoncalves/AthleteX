b2999dbe110334b5c6a2468d270723a48884471e
Jvgcgoncalves <goncalvesdacosta@gmail.com>
2023-06-27 11:40:15 -0300

    refactor: alter styles.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let mainDiv = document.getElementById('main-videos')
-let videos =  [`<iframe width="700" height="400" src="https://www.youtube.com/embed/LGCOYNQF_14" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,`<iframe width="700" height="400" src="https://www.youtube.com/embed/6F6ZLCcRDKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`]
+let videos =  [`<iframe class="videosToPlay" width="700" height="400" src="https://www.youtube.com/embed/LGCOYNQF_14" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,`<iframe class="videosToPlay" width="700" height="400" src="https://www.youtube.com/embed/6F6ZLCcRDKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`]
 let index = 0
 // Função de ir para o próximo vídeo
 function nextVideo(){
